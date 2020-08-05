ec943e19e3fa9c9b4cb4fdcee84caeca4f797931
orazweb <jer.paoli@gmail.com>
2020-08-05 14:37:26 +0200

    done

--- Shield.java
+++ Shield.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Shield {
 
@@ -13,10 +15,27 @@ public class Shield {
         names.add("Jemma Simmons");
 
         // TODO 1 : map names to agents list
-        List<Agent> agents = new ArrayList<>();
+//        List<Agent> agents = new ArrayList<>();
+//        première étape
+////        for (String name : names ){
+////            String[] test = name.split(" ");
+////            agents.add(new Agent(test[0],test[1]));
+////        }
+//      factorisation de la première étape
+//        for (String name : names ){
+////            String[] test = name.split(" ");
+//            agents.add(new Agent(name.split(" ")[0],name.split(" ")[1]));
+//        }
+
+
+        List<Agent> agents = names
+                .stream()
+                .map(item -> new Agent(item.split(" ")[0], item.split(" ")[1]))
+                .collect(Collectors.toList());
 
         showAgents(agents);
     }
+
 
     // don't touch anything here !
     private static void showAgents(List<Agent> agents) {
